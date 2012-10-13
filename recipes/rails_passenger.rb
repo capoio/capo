76658ed8ad42dcca0e25947bcb48adc0eb3bd62a
Joren <git@jorendegroof.be>
2012-10-13 10:55:32 +0200

    Adds deploy action to the passenger recipe

--- recipes/rails_passenger.rb
+++ recipes/rails_passenger.rb
@@ -1,6 +1,6 @@
 configs do
   description 'Passenger actions'
-  version 0.1
+  version 0.2
   tags %w[rails passenger]
   category 'rails'
   dependencies %w[]
@@ -10,5 +10,17 @@ namespace :passenger do
   desc 'Restart the web server'
   task :restart, :roles => :app do
     run "touch  #{deploy_to}/current/tmp/restart.txt"
+  end
+end
+
+namespace :deploy do
+  desc 'Restart your application'
+  task :restart do
+    passenger::restart
+  end
+
+  desc 'Start your application'
+  task :start do
+    passenger::restart
   end
 end
