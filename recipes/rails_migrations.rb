8cb9491c4daca1e34d34cd7e18bea6644ae9cbbc
Joren <git@jorendegroof.be>
2012-10-13 11:27:06 +0200

    Adds rails migration recipe

---
+++ recipes/rails_migrations.rb
@@ -0,0 +1,24 @@
+configs do
+  description 'Detect if rails migrations need to run'
+  version 0.1
+  tags %w[rails database migration]
+  category 'rails'
+  dependencies %w[]
+end
+
+desc 'Prompts if new migrations are available and runs them if you want to'
+  namespace :deploy do
+    task :needs_migrations, :roles => :db, :only => {:primary => true} do
+      migrations_changed = if previous_release.nil?
+        true # propably first deploy, so no migrations to compare
+      else
+        old_rev = capture("cd #{previous_release} && git log --pretty=format:'%H' -n 1 | cat").strip
+        new_rev = capture("cd #{latest_release} && git log --pretty=format:'%H' -n 1 | cat").strip
+        capture("cd #{latest_release} && git diff #{old_rev} #{new_rev} --name-only | cat").include?('db/migrate')
+      end
+      if migrations_changed && Capistrano::CLI.ui.ask("New migrations pending. Enter 'yes' to run db:migrate") == 'yes'
+        migrate
+      end
+    end
+  end
+  after 'deploy:update_code', 'deploy:needs_migrations'
