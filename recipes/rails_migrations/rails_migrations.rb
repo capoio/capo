2abb3da62aa5ff4ced5eef48c9f9a8e63afab3d2
Jeroen Jacobs <git@jeroenj.be>
2012-10-13 15:36:13 +0200

    [rails_migrations] Formatting: indenting

--- recipes/rails_migrations/rails_migrations.rb
+++ recipes/rails_migrations/rails_migrations.rb
@@ -1,16 +1,16 @@
 desc 'Prompts if new migrations are available and runs them if you want to'
-  namespace :deploy do
-    task :needs_migrations, :roles => :db, :only => {:primary => true} do
-      migrations_changed = if previous_release.nil?
-        true # propably first deploy, so no migrations to compare
-      else
-        old_rev = capture("cd #{previous_release} && git log --pretty=format:'%H' -n 1 | cat").strip
-        new_rev = capture("cd #{latest_release} && git log --pretty=format:'%H' -n 1 | cat").strip
-        capture("cd #{latest_release} && git diff #{old_rev} #{new_rev} --name-only | cat").include?('db/migrate')
-      end
-      if migrations_changed && Capistrano::CLI.ui.ask("New migrations pending. Enter 'yes' to run db:migrate") == 'yes'
-        migrate
-      end
+namespace :deploy do
+  task :needs_migrations, :roles => :db, :only => {:primary => true} do
+    migrations_changed = if previous_release.nil?
+      true # propably first deploy, so no migrations to compare
+    else
+      old_rev = capture("cd #{previous_release} && git log --pretty=format:'%H' -n 1 | cat").strip
+      new_rev = capture("cd #{latest_release} && git log --pretty=format:'%H' -n 1 | cat").strip
+      capture("cd #{latest_release} && git diff #{old_rev} #{new_rev} --name-only | cat").include?('db/migrate')
+    end
+    if migrations_changed && Capistrano::CLI.ui.ask("New migrations pending. Enter 'yes' to run db:migrate") == 'yes'
+      migrate
     end
   end
-  after 'deploy:update_code', 'deploy:needs_migrations'
+end
+after 'deploy:update_code', 'deploy:needs_migrations'
