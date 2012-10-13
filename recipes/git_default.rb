097bfd54d57b48e6d6e1509d1698550ecc2976d3
Joren <git@jorendegroof.be>
2012-10-13 11:41:39 +0200

    Adds scm pw possibilities

--- recipes/git_default.rb
+++ recipes/git_default.rb
@@ -14,5 +14,8 @@ set :git_enable_submodules, 1
 # Only deploy shallow clone, this will not clone the whole git history, just the latest state.
 set :git_shallow_clone, 1
 
+# Asks for a password for your repository but authentication via ssh is recmmended
+#set :scm_password, Proc.new {CLI.password_prompt 'GIT Password: '}
+
 # Set password for scm, using ssh key is recommended
 # set :scm_passphrase, "p@ssw0rd"  # The deploy user's password
--- recipes/svn_default.rb
+++ recipes/svn_default.rb
@@ -7,4 +7,9 @@ configs do
 end
 
 set :scm, :svn
-set :scm_password, Proc.new {CLI.password_prompt 'SVN Password: '}
+
+# Asks for a password for your repository but authentication via ssh is recmmended
+#set :scm_password, Proc.new {CLI.password_prompt 'SVN Password: '}
+
+# Set password for scm but using ssh key is recommended
+# set :scm_passphrase, "p@ssw0rd"  # The deploy user's password
