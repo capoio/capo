b6477a97fb956b285479791004d9302982893e85
Jeroen Jacobs <git@jeroenj.be>
2012-10-13 16:39:24 +0200

    [rails_env_configs] Configs block should be executed for all configs in the configs array

--- recipes/rails_env_configs/rails_env_configs.rb
+++ recipes/rails_env_configs/rails_env_configs.rb
@@ -2,25 +2,27 @@ unless exists?(:configs)
   set :configs, [:database]
 end
 
-namespace :configs do
-  namespace name do
-    desc "Create #{name}.yml in shared/config"
-    task :copy do
-      run "mkdir -p #{shared_path}/config"
+configs.each do |name|
+  namespace :configs do
+    namespace name do
+      desc "Create #{name}.yml in shared/config"
+      task :copy do
+        run "mkdir -p #{shared_path}/config"
 
-      CONFIG = YAML.load_file(%Q{./config/#{name}.yml})[rails_env.to_s]
-      file = "#{rails_env.to_s}:\n"
-      file << CONFIG.map{|key, value| "  #{key}: #{value}\n"}.join
+        CONFIG = YAML.load_file(%Q{./config/#{name}.yml})[rails_env.to_s]
+        file = "#{rails_env.to_s}:\n"
+        file << CONFIG.map{|key, value| "  #{key}: #{value}\n"}.join
 
-      put file, "#{shared_path}/config/#{name}.yml"
+        put file, "#{shared_path}/config/#{name}.yml"
+      end
+
+      desc "Link the #{name}.yml"
+      task :link do
+        run "ln -nfs #{shared_path}/config/#{name}.yml #{release_path}/config/#{name}.yml"
+      end
     end
 
-    desc "Link the #{name}.yml"
-    task :link do
-      run "ln -nfs #{shared_path}/config/#{name}.yml #{release_path}/config/#{name}.yml"
-    end
+    after 'deploy:setup', "configs:#{name}:copy"
+    after 'deploy:finalize_update', "configs:#{name}:link"
   end
-
-  after 'deploy:setup', "configs:#{name}:copy"
-  after 'deploy:finalize_update', "configs:#{name}:link"
 end
