d878a593c8ac8346161f14f739609d356fa6a4ca
Jeroen Jacobs <git@jeroenj.be>
2012-10-14 20:43:38 +0200

    [bundler] Adds exception when bundler gem could not be found

--- recipes/bundler/bundler.rb
+++ recipes/bundler/bundler.rb
@@ -4,4 +4,8 @@
 ###### End sets
 ##################################################
 
-require 'bundler/capistrano'
+begin
+  require 'bundler/capistrano'
+rescue LoadError => e
+  raise "\nbundler gem not found. Please install it with '(sudo) gem install bundler'."
+end
