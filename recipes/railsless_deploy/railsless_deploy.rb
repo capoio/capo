0b3163eed60d2c91ca88933b1ef33bc0107c04b9
Steven De Coeyer <steven@banteng.be>
2012-10-14 11:57:50 +0200

    Adds recipe for a railsless deploy

---
+++ recipes/railsless_deploy/railsless_deploy.rb
@@ -0,0 +1,5 @@
+begin
+  require 'railsless-deploy'
+rescue LoadError => e
+  raise "\nrailsless-deploy gem not found. Either install it with '(sudo) gem install railsless-deploy' of if you are using bundler add it to your gemfile."
+end
