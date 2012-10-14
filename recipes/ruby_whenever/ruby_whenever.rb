8ed0a15febc1528423f5c4db9778467e24a610da
Jeroen Jacobs <git@jeroenj.be>
2012-10-14 21:54:33 +0200

    [whenever] Fixes require

--- recipes/ruby_whenever/ruby_whenever.rb
+++ recipes/ruby_whenever/ruby_whenever.rb
@@ -7,7 +7,7 @@ set :whenever_command, "bundle exec whenever"
 ##################################################
 
 begin
-  require 'whenever'
+  require 'whenever/capistrano'
 rescue LoadError => e
   raise "\nwhenever gem not found. Either install it with '(sudo) gem install whenever' or add it to your Gemfile if you are using bundler."
 end
