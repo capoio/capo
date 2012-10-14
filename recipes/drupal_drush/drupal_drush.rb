5d8af5acb39f824e23b62f5d0d4ebdf0e0d3586f
Steven De Coeyer <steven@banteng.be>
2012-10-14 12:18:25 +0200

    Adds drupal drush recipe

---
+++ recipes/drupal_drush/drupal_drush.rb
@@ -0,0 +1,33 @@
+set :drush_cmd, 'drush_cmd'
+set :app_path, 'app_path'
+
+namespace :drush do
+
+  desc "Backup the database"
+  task :backupdb, :on_error => :continue do
+    run "#{drush_cmd} -r #{app_path} bam-backup"
+  end
+
+  desc "Run Drupal database migrations if required"
+  task :updatedb, :on_error => :continue do
+    run "#{drush_cmd} -r #{app_path} updatedb -y"
+  end
+
+  desc "Clear the drupal cache"
+  task :cache_clear, :on_error => :continue do
+    run "#{drush_cmd} -r #{app_path} cc all"
+  end
+
+  desc "Set the site offline"
+  task :site_offline, :on_error => :continue do
+    run "#{drush_cmd} -r #{app_path} vset site_offline 1 -y"
+    run "#{drush_cmd} -r #{app_path} vset maintenance_mode 1 -y"
+  end
+
+  desc "Set the site online"
+  task :site_online, :on_error => :continue do
+    run "#{drush_cmd} -r #{app_path} vset site_offline 0 -y"
+    run "#{drush_cmd} -r #{app_path} vset maintenance_mode 0 -y"
+  end
+
+end
