d0bef3c87dd6fbc4a95589e09522b433dab15062
Steven De Coeyer <steven@banteng.be>
2012-10-14 11:31:06 +0200

    Adds Drupal database backup recipe

---
+++ recipes/drupal_database_backup/drupal_database_backup.rb
@@ -0,0 +1,8 @@
+desc "Download a backup of the database(s) from the given stage."
+task :download_db, :roles => :db, :only => { :primary => true } do
+  domains.each do |domain|
+    filename = "#{domain}_#{stage}.sql"
+    run "#{drush} --uri=#{domain} sql dump --structure-tables-key=common > ~/#{filename}"
+    download("~/#{filename}", "db/#{filename}", :via=> :scp)
+  end
+end
