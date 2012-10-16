db9345ced9039638b25be5013530ee6b71d0e482
Jeroen Jacobs <git@jeroenj.be>
2012-10-16 21:56:21 +0200

    [rby_daemons] Adds recipe

---
+++ recipes/ruby_daemons/ruby_daemons.rb
@@ -0,0 +1,34 @@
+##################################################
+###### Begin sets
+
+unless exists?(:daemons)
+  set :daemons, [:dameon_name]
+end
+
+###### End sets
+##################################################
+
+daemons.each do |daemon|
+  namespace :mailman do
+    desc "#{daemon.capitalize}::Start"
+    task :start, :roles => [:app] do
+      run "cd #{current_path};RAILS_ENV=#{rails_env} bundle exec script/#{daemon}_daemon start"
+    end
+
+    desc "#{daemon.capitalize}::Stop"
+    task :stop, :roles => [:app] do
+      run "cd #{current_path};RAILS_ENV=#{rails_env} bundle exec script/#{daemon}_daemon stop"
+    end
+
+    desc "#{daemon.capitalize}::Restart"
+    task :restart, :roles => [:app] do
+      mailman.stop
+      mailman.start
+    end
+
+    desc "#{daemon.capitalize}::Status"
+    task :status, :roles => [:app] do
+      run "cd #{current_path};RAILS_ENV=#{rails_env} bundle exec script/#{daemon}_daemon status"
+    end
+  end
+end
