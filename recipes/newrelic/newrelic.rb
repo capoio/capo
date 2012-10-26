816e3daa2fa28c8dfcb7617d692387b47f998423
Jeroen Jacobs <git@jeroenj.be>
2012-10-26 17:25:05 +0200

    [newrelic] Puts variables (and require) in sets block

--- recipes/newrelic/newrelic.rb
+++ recipes/newrelic/newrelic.rb
@@ -1,9 +1,15 @@
+##################################################
+###### Begin sets
+
 begin
   require 'new_relic/recipes'
 rescue LoadError
   raise "\newrelic gem not found. Please install it with '(sudo) gem install newrelic'."
 end
 
-
 set :newrelic_appname, "YOUR_APP_NAME"
+
+###### End sets
+##################################################
+
 after "deploy:update", "newrelic:notice_deployment"
