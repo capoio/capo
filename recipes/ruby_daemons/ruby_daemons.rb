45d049c35019dfe1a1d8a9e5084c4e888454adcc
Jeroen Jacobs <git@jeroenj.be>
2013-01-04 13:42:23 +0100

    [ruby_daemons] Simple descriptions for capistrano tasks are enough

--- recipes/ruby_daemons/ruby_daemons.rb
+++ recipes/ruby_daemons/ruby_daemons.rb
@@ -14,13 +14,13 @@ daemons_per_role.each do |role, daemons|
   daemons.each do |daemon|
     namespace daemon do
       %w[start stop status].each do |command|
-        desc "#{daemon.capitalize}::#{command.capitalize}"
+        desc "#{daemon} #{command}"
         task command, :roles => [role] do
           run "cd #{current_path};RAILS_ENV=#{rails_env} bundle exec script/#{daemon}_daemon #{command}"
         end
       end
 
-      desc "#{daemon.capitalize}::Restart"
+      desc "#{daemon} restart"
       task :restart, :roles => [role] do
         send(daemon).stop
         send(daemon).start
