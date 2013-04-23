7adc3bbaae37ef2842f7074fb4a53e7019c360fc
Jeroen Jacobs <git@jeroenj.be>
2013-04-23 14:36:02 +0200

    [newrelic] Formatting: use single quotes when possible

--- recipes/newrelic/newrelic.rb
+++ recipes/newrelic/newrelic.rb
@@ -7,9 +7,9 @@ rescue LoadError
   raise "\nnewrelic_rpm gem not found. Please install it with '(sudo) gem install newrelic_rpm'."
 end
 
-set :newrelic_appname, "YOUR_APP_NAME"
+set :newrelic_appname, 'YOUR_APP_NAME'
 
 ###### End sets
 ##################################################
 
-after "deploy:update", "newrelic:notice_deployment"
+after 'deploy:update', 'newrelic:notice_deployment'
