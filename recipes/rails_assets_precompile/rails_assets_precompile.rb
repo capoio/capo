014e79c7ba1a991fbcf9ce8ef5c2d26213628d77
Jeroen Jacobs <git@jeroenj.be>
2013-08-16 16:28:19 +0200

    [rails_assets_precompile] Removes some comments

--- recipes/rails_assets_precompile/rails_assets_precompile.rb
+++ recipes/rails_assets_precompile/rails_assets_precompile.rb
@@ -10,8 +10,6 @@ namespace :deploy do
   namespace :assets do
     task :precompile, :roles => :web, :except => {:no_release => true} do
       changed = if previous_release.nil?
-        # Propably first deploy, so precompile them anyway.
-        # run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
         run <<-CMD.compact
           cd -- #{latest_release} &&
           RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} #{rake} assets:precompile
@@ -22,7 +20,6 @@ namespace :deploy do
         assets_changed = capture("cd #{latest_release} && git diff #{old_rev} #{new_rev} --name-only | cat").include?('asset')
         gemfile_changed = capture("cd #{latest_release} && git diff #{old_rev} #{new_rev} --name-only | cat").include?('Gemfile.lock')
         if assets_changed || (gemfile_changed && Capistrano::CLI.ui.ask("Gemfile changed. Enter 'yes' to precomple assets?") == 'yes')
-          # run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
           run <<-CMD.compact
             cd -- #{latest_release} &&
             RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} #{rake} assets:precompile
