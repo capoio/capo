f7a62d41fd447232bd006858826d3157e840b56c
Jeroen Jacobs <git@jeroenj.be>
2013-08-16 16:42:38 +0200

    [multistage] Adds recipe

---
+++ recipes/multistage/multistage.rb
@@ -0,0 +1,29 @@
+location = fetch :stage_dir, 'config/deploy/stages'
+
+unless exists?(:stages)
+  set :stages, Dir["#{location}/*.rb"].map { |f| File.basename f, '.rb' }
+end
+
+stages.each do |name|
+  desc "Set the target stage to `#{name}'."
+  task name do
+    set :stage, name.to_sym
+    load "#{location}/#{stage}"
+  end
+end
+
+namespace :multistage do
+  desc "[internal] Ensure that a stage has been selected."
+  task :ensure do
+    unless exists?(:stage)
+      if exists?(:default_stage)
+        logger.important "Defaulting to `#{default_stage}'"
+        find_and_execute_task default_stage
+      else
+        abort "No stage specified. Please specify one of: #{stages.join(', ')} (e.g. `cap #{stages.first} #{ARGV.last}')"
+      end
+    end
+  end
+end
+
+on :start, 'multistage:ensure', :except => stages + ['multistage:prepare']
